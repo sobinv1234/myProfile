62d45eacf6cac98cb89e069d6a97da08428d78d6
sobin <sobinv1234@gmail.com>
2026-04-20 11:03:23 +0100

    new portfolio updates 01

--- src/app/components/about/about.ts
+++ src/app/components/about/about.ts
@@ -12,7 +12,7 @@ import { CommonModule } from '@angular/common';
 export class AboutComponent {
   location = 'Southampton, United Kingdom';
   professionalSummary = `Results-driven Software Developer with over 7 years of experience in front-end development, specializing in React, Angular, JavaScript, and HTML5/CSS3. Proven ability to design, prototype (using Figma), and deliver responsive, high-performance web applications for large clients like Unilever and Aviva.`;
-  languages = ['English', 'Malayalam', 'Hindi'];
+  // languages = ['English', 'Malayalam', 'Hindi'];
   coreCompetencies = [
     'React', 'Angular', 'JavaScript (ES6+)', 'TypeScript',
     'HTML5', 'CSS3', 'Sass', 'Bootstrap',
